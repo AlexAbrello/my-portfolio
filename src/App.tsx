0e8fe2a6bcbc3a43c084c01df1ab47299a4fa121
AlexAbrello <abrello063@gmail.com>
2023-05-24 11:08:11 +0300

    Block 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import './App.css';
+import style from './App.module.css'
 import {Header} from "./components/Header/Header";
+import {Main} from "./components/Main/Main";
 
 function App() {
   return (
       <div>
         <Header/>
+        <div>
+          <Main />
+        </div>
       </div>
   );
 }
---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import style from './Main.module.css'
+import ava from './samurai.jpg'
+
+export const Main = () => {
+  return (
+      <div className={style.page}>
+        <div className={style.container}>
+          <div className={style.info}>
+            <div className={style.text}>
+              <div>Hi, my name is Aleksey Adamovich!</div>
+              <div>And I web-developer!</div>
+            </div>
+            <div>
+              <img src={ava} alt="Samurai" className={style.photo}/>
+            </div>
+          </div>
+        </div>
+      </div>
+  );
+};
+

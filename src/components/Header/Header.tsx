c8e25bc039aa220a2589bd6a56c2e0f59fd8826c
AlexAbrello <abrello063@gmail.com>
2023-05-24 10:25:10 +0300

    Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,8 +3,18 @@ import style from './Header.module.css'
 
 export const Header = () => {
   return (
-      <header className={style.container}>
-
+      <header className={style.header}>
+        <div className={style.container}>
+          <div className={style.wrapper}>
+            <div>My portfolio</div>
+            <nav className={style.navWrapper}>
+              <div>Main</div>
+              <div>My skills</div>
+              <div>My works</div>
+              <div>Contacts</div>
+            </nav>
+          </div>
+        </div>
       </header>
   );
 };

8e79a105f956ed695052e0392e3e5c56c92e146b
AlexAbrello <abrello063@gmail.com>
2023-07-10 17:27:48 +0300

    new portfolio maim (photo)

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,21 +1,13 @@
 import React from 'react';
 import style from './Main.module.css'
-import ava from './samurai.jpg'
+import ava from '../../assets/frontend.png'
 
 export const Main = () => {
   return (
       <div className={style.page}>
-        <div className={style.container}>
-          <div className={style.info}>
-            <div className={style.text}>
-              <div>Hi, my name is Aleksey Adamovich!</div>
-              <div>And I'm a web-developer!</div>
-            </div>
             <div>
-              <img src={ava} alt="Samurai" className={style.photo}/>
+              <img src={ava} alt="Man" className={style.photo}/>
             </div>
-          </div>
-        </div>
       </div>
   );
 };

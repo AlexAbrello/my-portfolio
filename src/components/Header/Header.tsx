e3b68b1822c865a26c5b4cfa17bf17d6b73ee8a0
AlexAbrello <abrello063@gmail.com>
2023-07-10 11:17:44 +0300

    new portfolio header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,33 @@
 import React from 'react';
 import style from './Header.module.css'
+import {FaHome} from "react-icons/fa";
+import {FaBrain} from "react-icons/fa";
+import {FaBriefcase} from "react-icons/fa";
+import {FaPhone} from "react-icons/fa";
 
 export const Header = () => {
   return (
       <header className={style.header}>
         <nav>
-          <div className={style.navIcon}>
-            <a href='#'>Main</a>
+          <div className={style.nav}>
+            <a href='#'>
+              <div className={style.navIcon}><FaHome/></div>
+              Main</a>
           </div>
-          <div className={style.navIcon}>
-            <a href='#'>My skills</a>
+          <div className={style.nav}>
+            <a href='#'>
+              <div className={style.navIcon}><FaBrain/></div>
+              My skills</a>
           </div>
-          <div className={style.navIcon}>
-            <a href='#'>My works</a>
+          <div className={style.nav}>
+            <a href='#'>
+              <div className={style.navIcon}><FaBriefcase/></div>
+              My works</a>
           </div>
-          <div className={style.navIcon}>
-            <a href='#'>Contacts</a>
+          <div className={style.nav}>
+            <a href='#'>
+              <div className={style.navIcon}><FaPhone/></div>
+              Contacts</a>
           </div>
         </nav>
       </header>

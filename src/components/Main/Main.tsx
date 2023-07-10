11eff721f6d1c1810b7ec232d144ab17fa787d13
AlexAbrello <abrello063@gmail.com>
2023-07-10 18:43:25 +0300

    new portfolio main

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import style from './Main.module.css'
 import ava from '../../assets/frontend.png'
+import {Button} from "../Button/Button";
 
 export const Main = () => {
   return (
@@ -9,10 +10,19 @@ export const Main = () => {
           <div className={style.textWrapper}>
             <h6 className={style.textHi}>Hello there...</h6>
             <h1 className={style.textName}>Aleksey Adamovich</h1>
-            <p className={style.text}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, corporis dolore eligendi est explicabo fuga
+            <p className={style.text}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, corporis dolore
+              eligendi est explicabo fuga
               itaque laboriosam molestiae tempore unde. Alias architecto consequatur culpa, cumque doloribus eos facilis
               fugit iusto laudantium minus nostrum perferendis possimus repellendus sed similique tempore temporibus. Ea
               earum enim expedita ipsum labore nulla placeat quaerat ratione!</p>
+          </div>
+          <div className={style.buttonWrapper}>
+            <div className={style.buttonMyWork}>
+              <Button name={'My Works'}/>
+            </div>
+            <div className={style.buttonHireMe}>
+              <Button name={'Hire Me'}/>
+            </div>
           </div>
         </div>
         <img src={ava} alt="Man" className={style.photo}/>

da13575c6384e48445688767a9adcd0d11a10ee8
AlexAbrello <abrello063@gmail.com>
2023-07-11 17:47:26 +0300

    new portfolio skills (experience)

--- src/components/My Skills/MySkills.tsx
+++ src/components/My Skills/MySkills.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import style from './MySkills.module.css'
 import {Headline} from "../Headline/Headline";
-import {FaCss3Alt, FaHtml5, FaReact} from "react-icons/fa";
+import {FaCss3Alt, FaGithub, FaHtml5, FaReact} from "react-icons/fa";
 import {IoLogoJavascript} from "react-icons/io5";
 import {TbBrandRedux} from "react-icons/tb";
+import {SiTypescript} from "react-icons/si";
 
 
 export const MySkills = () => {
@@ -28,6 +29,10 @@ export const MySkills = () => {
                 <p className={style.stackText}>JavaScript</p>
               </div>
               <div>
+                <SiTypescript style={{color: '#208fcb', fontSize: '70px', margin: '0 65px 0'}}/>
+                <p className={style.stackText}>TypeScript</p>
+              </div>
+              <div>
                 <FaReact style={{color: '#00baff', fontSize: '70px', margin: '0 65px 0'}}/>
                 <p className={style.stackText}>React</p>
               </div>
@@ -35,12 +40,25 @@ export const MySkills = () => {
                 <TbBrandRedux style={{color: '#8300ff', fontSize: '70px', margin: '0 65px 0'}}/>
                 <p className={style.stackText}>Redux</p>
               </div>
+              <div>
+                <FaGithub style={{color: '#2f2c31', fontSize: '70px', margin: '0 65px 0'}}/>
+                <p className={style.stackText}>GitHub</p>
+              </div>
             </div>
           </div>
           <div style={{textAlign: 'end', marginTop: '20px'}}>
             <Headline name={'Experience'}/>
           </div>
-
+          <div className={style.description}>
+            <ul>
+              <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste, optio!</li>
+              <li>Lorem ipsum dolor sit amet.</li>
+              <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, architecto at consequuntur culpa, error
+                expedita harum ipsam laboriosam natus numquam quam saepe sunt unde voluptates.
+              </li>
+              <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium dolor itaque nobis odio ratione voluptas?</li>
+            </ul>
+          </div>
         </div>
       </div>
   );

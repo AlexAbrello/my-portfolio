d68299a6b8e90384f71c03fa18644fc19add6fa3
AlexAbrello <abrello063@gmail.com>
2023-07-12 15:51:17 +0300

    new portfolio

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
 import style from './Footer.module.css'
+import {SlSocialVkontakte} from "react-icons/sl";
+import {LiaTelegram} from "react-icons/lia";
+import {AiOutlineLinkedin} from "react-icons/ai";
+import {MdOutlineDocumentScanner} from "react-icons/md";
+import {FiGithub} from "react-icons/fi";
 
 export const Footer = () => {
   return (
       <div className={style.page}>
-        <div className={style.container}>
-          <div className={style.info}>
-            <div className={style.title}>Aleksey Adamovich</div>
-            <div className={style.networks}>
-              <a href="#" className={style.networksItem}>Vkontakte</a>
-              <a href="#" className={style.networksItem}>Telegram</a>
-              <a href="#" className={style.networksItem}>WhatsApp</a>
-              <a href="#" className={style.networksItem}>Discord</a>
-            </div>
+        <div className={style.text}>Aleksey Adamovich</div>
+        <div className={style.icons}>
+          <div className={style.iconsItem}>
+            <SlSocialVkontakte />
+          </div>
+          <div className={style.iconsItem}>
+            <LiaTelegram />
+          </div>
+          <div className={style.iconsItem}>
+            <AiOutlineLinkedin />
+          </div>
+          <div className={style.iconsItem}>
+            <MdOutlineDocumentScanner />
+          </div>
+          <div className={style.iconsItem}>
+            <FiGithub />
           </div>
         </div>
       </div>
